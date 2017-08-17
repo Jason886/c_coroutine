f81642611e67fff785ae63e389790f0dea335128
jason886 <359152155@qq.com>
2017-08-17 18:22:33 +0800

    delete fiels

--- context_coroutine/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-CFLAGS=-I.
-gcc -D_GNU_SOURCE -pedantic -std=c89 -c coroutine.c -o coroutine.o
-gcc -D_GNU_SOURCE -pedantic -std=c89 -c test.c -o test.o
-gcc -o test test.o coroutine.o
-rm -rf *.o
